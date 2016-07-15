1c5af7a071e5ebe8495efde35fd052e1f569c7e6
natansevero <natanseverolp@gmail.com>
2016-07-15 15:31:22 -0300

    Tentando corrigir erros quando criar o arquivo

--- capture.js
+++ capture.js
@@ -6,4 +6,21 @@ $("#button_down").click(function(){
   console.log(code_editor.value);
   //$("div").append('"' + code_editor.value + '"');
   $("div").append(code_editor.value);
+  download("opa", 'testfile.txt', 'text/pain');
+  // createFile(code_editor.value);
 });
+
+
+function download(text, name, type) {
+  var a = document.getElementById("a");
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+}
+
+// function createFile(code){
+//   var fso = CreateObject("Scripting.FileSystemObject");
+//   var a = fso.CreateTextFile("/home/natan/Área de Trabalho/testfile.html", true);
+//   a.writeline(code);
+//   a.Close();
+// }
