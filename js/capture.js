eadb377b11f636238f9ae29b85f08ef1c77a2e81
natansevero <natanseverolp@gmail.com>
2016-09-29 12:04:49 -0300

    Adicionando package.json no projeto e fazendo testes com o require. Tbm adicioando o FileSystem

--- js/capture.js
+++ js/capture.js
@@ -1,9 +1,20 @@
-var code_editor = document.getElementById('code_editor');
-//var button_down = document.getElementById('button_down');
+define(function(require){
+  const fs = require('fs');
+  var code_editor = document.getElementById('code_editor');
+  var button_down = document.getElementById('button_down');
+ 
+  const file = 'code.html';
+  const callback = (err) => {
+    if(err) throw err
+    else console.log("Download feito com Sucesso");
+  }
 
+  $("#button_down").click(function(){
+    console.log(code_editor.value);
+    const data = code_editor.value;
+    //$("div").append('"' + code_editor.value + '"');
+    $("div").append(code_editor.value);
 
-$("#button_down").click(function(){
-  console.log(code_editor.value);
-  //$("div").append('"' + code_editor.value + '"');
-  $("div").append(code_editor.value);
+    fs.writeFile(file, data, callback);
+  });
 });
